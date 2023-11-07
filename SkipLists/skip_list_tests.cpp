f9c816753bf0cd218b7846d31aa4a8612b8e0f4c
Isalia20 <irakli.salia854@gmail.com>
2023-11-07 22:55:05 +0400

    building tests

--- SkipLists/skip_list_tests.cpp
+++ SkipLists/skip_list_tests.cpp
@@ -1,7 +1,9 @@
 #include "SkipList.h"
 #include <random>
 
-void check_build_func_deterministic(){
+using namespace std;
+
+void check_build_func_large(bool is_random_skip_list){
     // Create a vector with numbers from 1 to 1,000,000
     std::vector<int> numbers;
     for (int i = 1; i <= 1000000; ++i) {
@@ -15,41 +17,62 @@ void check_build_func_deterministic(){
     // Shuffle the vector
     std::shuffle(numbers.begin(), numbers.end(), g);
 
-    // Print first 10 numbers from the shuffled vector
     SkipList sl;
-    sl.build_skip_list_deterministic(numbers);
+    if (is_random_skip_list){
+        sl.build_skip_list_random(numbers);
+    } else {
+        sl.build_skip_list_deterministic(numbers);
+    }
 }
 
-void check_build_func_random(){
-    // Create a vector with numbers from 1 to 1,000,000
-    std::vector<int> numbers;
-    for (int i = 1; i <= 1000000; ++i) {
-        numbers.push_back(i);
-    }
+void check_build_func_small(bool is_random_skip_list){
+    // Create a vector with small amount of numbers
+    std::vector<int> numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 
-    // Create a random device and generator
-    std::random_device rd;
-    std::mt19937 g(rd());
-    
-    // Shuffle the vector
-    std::shuffle(numbers.begin(), numbers.end(), g);
-
-    // Print first 10 numbers from the shuffled vector
     SkipList sl;
-    sl.build_skip_list_random(numbers);
+    if (is_random_skip_list){
+        sl.build_skip_list_random(numbers);
+        cout << "SKIP LIST RANDOM SMALL" << endl;
+        sl.print();
+    } else {
+        sl.build_skip_list_deterministic(numbers);
+        cout << "SKIP LIST DETERMINISTIC SMALL" << endl;
+        sl.print();
+    }
+}
+
+void check_build_func_one(bool is_random_skip_list){
+    // Create a vector with a single element
+    std::vector<int> numbers = {1};
+
+    SkipList sl;
+    sl.build_skip_list_deterministic(numbers);
+    if (is_random_skip_list){
+        sl.build_skip_list_random(numbers);
+        cout << "SKIP LIST RANDOM ONE" << endl;
+        sl.print();
+    } else {
+        sl.build_skip_list_deterministic(numbers);
+        cout << "SKIP LIST DETERMINISTIC ONE" << endl;
+        sl.print();
+    }
 }
 
 bool check_build_funcs(){
+    // Multiple times just to make sure nothing goes wrong on large vectors
     for (int i = 0; i < 10; i++){
         cout << i << endl;
-        check_build_func_deterministic();
-        check_build_func_random();
+        check_build_func_large(true);
+        check_build_func_large(false);
     }
+    check_build_func_small(true);
+    check_build_func_small(false);
+    check_build_func_one(true);
+    check_build_func_one(true);
     return true;
 }
 
 int main() {
     check_build_funcs();
-
     return 0;
 }
