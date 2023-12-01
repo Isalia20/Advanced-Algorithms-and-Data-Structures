e324b76b643f61c3acd49978b09b095711fd1277
irakli_salia <Irakli_Salia@epam.com>
2023-12-01 16:56:45 +0400

    parameters for visualization

---
+++ SkipLists/visualization_parameters.py
@@ -0,0 +1,130 @@
+VISUALIZATION_PARAMETERS = {
+    "last_element": [
+        {
+            'file_name': 'statistical_comparisons/time_results_search_last_deterministic_100000000.txt', 
+            'index': 0, 
+            'title': '100 million deterministic last element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_last_deterministic_10000000.txt', 
+            'index': 2, 
+            'title': '10 million deterministic last element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_last_deterministic_1000000.txt', 
+            'index': 4, 
+            'title': '1 million deterministic last element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_last_random_100000000.txt', 
+            'index': 1, 
+            'title': '100 million random last element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_last_random_10000000.txt', 
+            'index': 3, 
+            'title': '10 million random last element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_last_random_1000000.txt', 
+            'index': 5, 
+            'title': '1 million random last element'
+        },
+    ],
+    "quarter_element": [
+        {
+            'file_name': 'statistical_comparisons/time_results_search_quarter_deterministic_100000000.txt', 
+            'index': 0, 
+            'title': '100 million deterministic quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_quarter_deterministic_10000000.txt', 
+            'index': 2, 
+            'title': '10 million deterministic quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_quarter_deterministic_1000000.txt', 
+            'index': 4, 
+            'title': '1 million deterministic quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_quarter_random_100000000.txt', 
+            'index': 1, 
+            'title': '100 million random quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_quarter_random_10000000.txt', 
+            'index': 3, 
+            'title': '10 million random quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_quarter_random_1000000.txt', 
+            'index': 5, 
+            'title': '1 million random quarter element'
+        },
+    ], 
+    "third_quarter": [
+        {
+            'file_name': 'statistical_comparisons/time_results_search_third_quarter_deterministic_100000000.txt', 
+            'index': 0, 
+            'title': '100 million deterministic third_quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_third_quarter_deterministic_10000000.txt', 
+            'index': 2, 
+            'title': '10 million deterministic third_quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_third_quarter_deterministic_1000000.txt', 
+            'index': 4, 
+            'title': '1 million deterministic third_quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_third_quarter_random_100000000.txt', 
+            'index': 1, 
+            'title': '100 million random third_quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_third_quarter_random_10000000.txt', 
+            'index': 3, 
+            'title': '10 million random third_quarter element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_third_quarter_random_1000000.txt', 
+            'index': 5, 
+            'title': '1 million random third_quarter element'
+        },
+    ],
+    "mid": [
+        {
+            'file_name': 'statistical_comparisons/time_results_search_mid_deterministic_100000000.txt', 
+            'index': 0, 
+            'title': '100 million deterministic mid element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_mid_deterministic_10000000.txt', 
+            'index': 2, 
+            'title': '10 million deterministic mid element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_mid_deterministic_1000000.txt', 
+            'index': 4, 
+            'title': '1 million deterministic mid element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_mid_random_100000000.txt', 
+            'index': 1, 
+            'title': '100 million random mid element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_mid_random_10000000.txt', 
+            'index': 3, 
+            'title': '10 million random mid element'
+        },
+        {
+            'file_name': 'statistical_comparisons/time_results_search_mid_random_1000000.txt', 
+            'index': 5, 
+            'title': '1 million random mid element'
+        },
+    ]
+}
