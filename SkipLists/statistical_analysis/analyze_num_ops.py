50aa331354fcf0d26cebaf504ebe27f2aa8a50fe
irakli_salia <Irakli_Salia@epam.com>
2023-12-11 22:04:52 +0400

    new analyze files and 2 new images for search query analysis

---
+++ SkipLists/statistical_analysis/analyze_num_ops.py
@@ -0,0 +1,65 @@
+from typing import Dict
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def parse_num_ops_txt(num_ops_file_path: str):
+    element_num_ops = {}
+    
+    with open(num_ops_file_path, 'r') as f:
+        op_count_sum = 0
+        for line in f:
+            line = line.replace("\n", "")
+            if "ELEMENT" in line:
+                element_num = line.split("-")[1].replace(" ", "")
+                element_num_ops[element_num] = 0
+            if line == "------------------------------------": # End of skip list
+                element_num_ops[element_num] = op_count_sum
+                op_count_sum = 0
+                continue
+            op_count_sum += 1
+    return element_num_ops
+
+def get_counts(data_structure_dict: Dict[str, int], indices):
+    counts = [data_structure_dict.get(str(i), 0) for i in indices]
+    return counts
+
+def bin_data(data, bin_edges):
+    binned_data = np.empty(len(bin_edges) - 1)
+    for i in range(len(bin_edges) - 1):
+        bin_range = data[bin_edges[i]:bin_edges[i + 1]]
+        binned_data[i] = np.mean(bin_range)  # Using mean for bin summary
+    return binned_data
+
+def plot_bst_skiplist(bin_centers, bst_binned, skiplist_binned):
+    plt.figure(figsize=(10, 5)) 
+
+    plt.plot(bin_centers, bst_binned, label='BST Operation Count', color='blue')
+    plt.plot(bin_centers, skiplist_binned, label='SkipList Operation Count', color='red')
+
+    # Labeling the plot
+    plt.title('Binned Operation Count Comparison between BST and SkipList')
+    plt.xlabel('Number to search')
+    plt.ylabel('Operation Count')
+    plt.legend()
+    plt.show()
+    return
+
+def main():
+    indices = np.arange(0, 100002)
+    bst = parse_num_ops_txt("statistical_comparisons/count_ops_bst.txt")
+    skiplist = parse_num_ops_txt("statistical_comparisons/count_ops_skiplist.txt")
+    bst_counts = get_counts(bst, indices)
+    skiplist_counts = get_counts(skiplist, indices)
+
+    bin_size = 20
+    bin_edges = np.arange(0, len(bst_counts), bin_size)
+    bin_centers = bin_edges[:-1] + np.diff(bin_edges) / 2
+    bst_binned = bin_data(bst_counts, bin_edges)
+    skiplist_binned = bin_data(skiplist_counts, bin_edges)
+    plot_bst_skiplist(bin_centers, bst_binned, skiplist_binned)
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ SkipLists/statistical_analysis/analyze_range_query.py
@@ -0,0 +1,53 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def parse_range_query_txt(path_to_txt: str):
+    all_times = []
+    with open(path_to_txt, 'r') as f:
+        for line in f:
+            line = line.replace("\n", "")
+            time = int(line)
+            all_times.append(time)
+    return all_times
+
+def get_times(range_query_bst_path: str, range_query_sl_path: str):
+    bst_times = parse_range_query_txt(range_query_bst_path)
+    sl_times = parse_range_query_txt(range_query_sl_path)
+
+    # Calculate means and standard deviations
+    bst_mean = np.mean(bst_times)
+    sl_mean = np.mean(sl_times)
+    sl_std = np.std(sl_times)
+    bst_std = np.std(bst_times)
+
+    # 3-sigma rule
+    bst_left, bst_right = bst_mean - bst_std * 3, bst_mean + bst_std * 3
+    sl_left, sl_right = sl_mean - sl_std * 3, sl_mean + sl_std * 3
+
+    # Filter out the values
+    bst_times = [i for i in bst_times if i > bst_left and i < bst_right]
+    sl_times = [i for i in sl_times if i > sl_left and i < sl_right]
+    return bst_times, sl_times
+
+def plot_times(bst_times, sl_times):
+    sns.histplot(data=bst_times, kde=True, color="blue", label="BST")
+    sns.histplot(data=sl_times, kde=True, color="green", label="SkipList")
+    plt.title("SkipList vs BST Range Query(500-10000), 1M Array")
+    plt.xlabel("Time taken to print all numbers in the range")
+    plt.ylabel("# Count")
+    plt.legend()
+    plt.show()
+
+def main():
+    (bst_times,
+     sl_times,
+     ) = get_times("statistical_comparisons/range_query_bst.txt", "statistical_comparisons/range_query_sl.txt")
+    bst_times = np.array(bst_times)
+    sl_times = np.array(sl_times)
+    plot_times(bst_times, sl_times)
+
+if __name__ == "__main__":
+    main()
+    
