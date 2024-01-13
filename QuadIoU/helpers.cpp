82b439b49e5ee6322ff75e6e140bb1de11665c09
isalia20 <irakli.salia854@gmail.com>
2024-01-13 15:06:59 +0400

    get rid of useless files(not torch compatible)

--- QuadIoU/helpers.cpp
+++
@@ -1,7 +0,0 @@
-#include "quad_iou.cpp"
-
-void printQuadrilateral(const Quadrilateral& quad) {
-    for (int i = 0; i < 4; ++i) {
-        std::cout << "Corner " << i+1 << ": (" << quad.corners[i].x << ", " << quad.corners[i].y << ")\n";
-    }
-}
